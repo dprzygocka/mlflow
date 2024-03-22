34945366cc0f579b5f8562063b4e6be2f3badd7b
dprzygocka <przygocka.dagmara@gmail.com>
2024-03-22 12:35:10 +0100

    test

--- mlflow/store/db/utils.py
+++ mlflow/store/db/utils.py
@@ -335,4 +335,4 @@ def create_sqlalchemy_engine(db_uri):
     print("db_uri", db_uri)
     print('ask sql alchemy to create engine based on uri')
     print('how sqlalchemy know how to create duckdb engine??? bc i installed duckdb-engine created by mouse')
-    return sqlalchemy.create_engine(db_uri, pool_pre_ping=True, **pool_kwargs)
+    return sqlalchemy.create_engine(db_uri, pool_pre_ping=True, **pool_kwargs, connect_args={'read_only': True,})
